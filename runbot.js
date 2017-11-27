199960b483df78e5b4935c4bfeb9c84ee06fc8ef
Ajdin Alic <ajdin1997@users.noreply.github.com>
2017-11-27 15:24:31 +0100

    Added su explanation.

--- runbot.js
+++ runbot.js
@@ -4788,6 +4788,20 @@
                     }
                 }
             },
+		
+	 suCommand: {
+                command: 'su',
+                rank: 'user',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!bBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        API.sendChat("/me @" + chat.un + " SU znaci korisnici sa superuser pristupom.");
+                    }
+                }
+            },
+		
              //Balkan Party commands
             eldoxCommand: {
 command: 'eldox',
